a6e059f68842c5a52de04fd6fc450970c58bdfb0
comjun04 <30339539+comjun04@users.noreply.github.com>
2018-11-16 22:50:40 +0900

    added sample list & fixed screen size issues

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
+// select screen height
+var selectScreenHeight = window.innerHeight - document.getElementById('selectTopBar').offsetHeight;
+document.getElementById('selectScreen').style.height = selectScreenHeight + "px";
+document.getElementById('selectList').style.height = (selectScreenHeight - 20) + "px";// Number = top/bottom padding
+
+// Back to index.html
 function gotoHome() {
 	if(confirm("게임을 종료하고 초기 화면으로 돌아갈까요?")) {
 		window.location.replace("index.html");
